9c3005933950573579eb630817f0f749aa401e1a
Allen Clineff <allen.clineff@lifeomic.com>
2021-09-22 15:46:11 -0400

    Failing attempt at .test.tsx babel compilation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.tsx",
-  devtool: "inline-source-map",
-  mode: "development",
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  mode: 'development',
   module: {
     rules: [
       {
+        // Compile source code with ts
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
+      },
+      {
+        // jest requires fallback to babel
+        test: /\.test\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-typescript', '@babel/preset-react'],
+          },
+        },
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "React from scratch",
+      title: 'React from scratch',
       templateContent: () =>
         `<!DOCTYPE html>
         <html>
@@ -33,7 +45,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
 };
